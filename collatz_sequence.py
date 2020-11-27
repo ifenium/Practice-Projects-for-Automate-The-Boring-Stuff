9320d03574835967d88bcd29169c9ae201e314ca
ife <oyeife@gmail.com>
2020-11-27 11:41:56 +0100

    Solution complete

---
+++ collatz_sequence.py
@@ -0,0 +1,11 @@
+def collatz(number):
+    if number % 2 == 0: 
+        print(number // 2)
+        return number // 2
+    else:   
+        print(3 * number + 1)
+        return 3 * number + 1
+        
+num = int(input('Enter a number: '))
+while num != 1:
+    num = collatz(num)
