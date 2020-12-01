862ce092a86f605170f7732201ed2e8eeff52986
ife <oyeife@gmail.com>
2020-12-01 11:17:22 +0100

    Added Exception Handling for Index Error ( When the user provides an empty list).

--- comma_code.py
+++ comma_code.py
@@ -1,9 +1,12 @@
 spam = ['apples', 'bananas', 'tofu', 'cats']
 
 def comma_code(lists):
-    if len(lists) == 1:
-        return lists[0]
-    else:
-        return '{}, and {}'. format(', '.join(lists[:-1]), lists[-1])
+    try: 
+        if len(lists) == 1:
+            return lists[0]
+        else:
+            return '{}, and {}'. format(', '.join(lists[:-1]), lists[-1])
+    except IndexError:
+        print('Index Error, you passed an empty list')
 
 print(comma_code(spam))
